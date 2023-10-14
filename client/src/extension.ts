4892641c1e2675c36d5afd23963ae977d3bf0910
Taichi Maeda <taichi.maeda.up@gmail.com>
2023-10-14 04:29:03 +0100

    feat: Use golang server instead of node server module

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -3,42 +3,41 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import { window, ExtensionContext } from 'vscode';
 
 import {
 	LanguageClient,
 	LanguageClientOptions,
 	ServerOptions,
-	TransportKind
 } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	const serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-		}
+		command: "go",
+		args: [
+			"run",
+			"/Users/taichimaeda/workspace/projects/freee/wireplus/cmd/wireplus/main.go",
+			'lsp',
+			'2>stderr.log',
+			"|",
+			"tee",
+			"/Users/taichimaeda/workspace/projects/freee/wireplus/stdout.log"
+		],
+		options: {
+			cwd: '/Users/taichimaeda/workspace/projects/freee/wireplus',
+			shell: true,
+		},
 	};
 
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
+		documentSelector: [{ scheme: 'file', language: 'go' }],
+		outputChannel: window.createOutputChannel('Example Language Server'),
 	};
 
 	// Create the language client and start the client.
